a926951fbdf77ffd9b25bd6045b1945b640fa5e9
requinard <d.diks94@gmail.com>
2017-08-09 14:11:46 +0200

    feat(bot): add fiftyfifty command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import time
+from random import Random
 
+import praw
 import telepot
 from telepot.loop import MessageLoop
 
@@ -25,17 +27,45 @@ def handle(msg):
         if text.startswith('/'):
             pass
         elif chat_id in chatlogs.keys():
-            chatlogs[chat_id] += [text,]
+            chatlogs[chat_id] += [text, ]
         else:
-            chatlogs[chat_id] = [text,]
+            chatlogs[chat_id] = [text, ]
 
-        if text == "/r/fiftyfifty":
-            bot.sendMessage(chat_id, "50/50")
-        elif text.startswith("/repeat"):
+        if text == "/fiftyfifty":
+            bot.sendMessage(chat_id, "Fetching")
+            reddit = get_reddit()
+            for submission in reddit.subreddit('fiftyfifty').new(limit=10):
+                print(submission.title)
+                if Random().randint(0, 5) == 1:
+                    bot.sendMessage(chat_id, "{0}: {1}".format(submission.title, submission.url),
+                                    disable_web_page_preview=True)
+        elif text == "/help" or text == "/start":
+            bot.sendMessage(chat_id, """
+            From the depths of hades we have retrieved ShibeBot
+            
+**Glossary**
+- /save <n>; Save the last N messages to the eurosquad subreddit
+- /fiftyfifty; Grab a random post from fiftyfifty
+            """)
+        elif text.startswith("/save"):
             n = int(tokens[1])
-            bot.sendMessage(chat_id, "\n".join(chatlogs[chat_id][:n:-1]))
+            if n is None or n > len(chatlogs[chat_id]):
+                bot.sendMessage(chat_id, "Maybe try working me like I'm supposed to")
+            else:
+                saved_logs = chatlogs[chat_id][:n:-1]
+                get_reddit().post
+                bot.sendMessage(chat_id, "\n".join(saved_logs))
+
 
 chatlogs = {}
+
+
+def get_reddit():
+    return praw.Reddit(
+        client_id=sys.argv[2],
+        client_secret=sys.argv[3],
+        user_agent="Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"
+    )
 
 
 TOKEN = sys.argv[1]  # get token from command-line
